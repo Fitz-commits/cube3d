a48e117f9b4410b80e7f00d2dabe86c60816a8b9
Christopher Delaby <cdelaby@w-r9-p4.s19.be>
2020-01-09 19:48:13 +0100

    map in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdelaby <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/12 13:47:22 by cdelaby           #+#    #+#             */
-/*   Updated: 2019/12/12 14:22:03 by cdelaby          ###   ########.fr       */
+/*   Updated: 2020/01/09 19:44:43 by cdelaby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,24 @@
 #include "cube3d.h"
 
 /*int		main()
-{
-	void *mlx;
-	void *win_ptr;
-	int i;
-	int j = 200;
-	i = 200;
-	mlx = mlx_init();
-	win_ptr = mlx_new_window (mlx, 1920, 1080, "hello" );
-	while (j < 500)
-	{
-		i = 200;
-		while(i < 600)
-			mlx_pixel_put (mlx, win_ptr, i++, j, 16777215);
-		j++;
-	}
-	mlx_loop(mlx);
-	return (0);
-}*/
+  {
+  void *mlx;
+  void *win_ptr;
+  int i;
+  int j = 200;
+  i = 200;
+  mlx = mlx_init();
+  win_ptr = mlx_new_window (mlx, 1920, 1080, "hello" );
+  while (j < 500)
+  {
+  i = 200;
+  while(i < 600)
+  mlx_pixel_put (mlx, win_ptr, i++, j, 16777215);
+  j++;
+  }
+  mlx_loop(mlx);
+  return (0);
+  }*/
 
 int		main()
 {
@@ -44,6 +44,7 @@ int		main()
 	inf = initSpec("./map.cub");
 	mlx = mlx_init();
 	win_ptr = mlx_new_window (mlx, 480, 270, "Projet du turfu" );
+	inf = initSpec("./map.cub")
 
 	double posX = 2, posY = 1; // Vecteur position
 	double dirX = -1, dirY = 0; // Vecteur direction
@@ -53,30 +54,52 @@ int		main()
 	double oldTime = 0; //time of previous frame
 
 	for(int x = 0; x < 270; x++)
-    {
-    	//calculate ray position and direction
-    	double cameraX = 2 * x / (double)(270) - 1; //x-coordinate in camera space
-    	double rayDirX = dirX + planeX * cameraX;
-    	double rayDirY = dirY + planeY * cameraX;
+	{
+		//calculate ray position and direction
+		double cameraX = 2 * x / (double)(270) - 1; //x-coordinate in camera space
+		double rayDirX = dirX + planeX * cameraX;
+		double rayDirY = dirY + planeY * cameraX;
 
 		//which box of the map we're in
 		int mapX = (int)(posX);
-      	int mapY = (int)(posY);
+		int mapY = (int)(posY);
 
 		//length of ray from current position to next x or y-side
 		double sideDistX;
-      	double sideDistY;
-		
+		double sideDistY;
+
 		//length of ray from one x or y-side to next x or y-side
 		double deltaDistX = fabs(1 / rayDirX);
-      	double deltaDistY = fabs(1 / rayDirY);
-      	double perpWallDist;
-		
-		//what direction to step in x or y-direction (either +1 or -1)
-      	int stepX;
-      	int stepY;
+		double deltaDistY = fabs(1 / rayDirY);
+		double perpWallDist;
 
-      	int hit = 0; //was there a wall hit?
-      	int side; //was a NS or a EW wall hit?
+		//what direction to step in x or y-direction (either +1 or -1)
+		int stepX;
+		int stepY;
+
+		int hit = 0; //was there a wall hit?
+		int side; //was a NS or a EW wall hit?
+
+		//calculate step and initial sideDist
+		if (rayDirX < 0)
+		{
+			stepX = -1;
+			sideDistX = (posX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+		}
+		if (rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (posY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+		}
 	}
 }
